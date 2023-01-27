0e24525704727c20c3e14733894979e082d9a766
HanquanL <hanquanl0115@gmail.com>
2023-01-27 18:26:11 -0500

    user entity and mysql connection

---
+++ src/main/java/com/timecapsule/app/time/capsule/entity/User.java
@@ -0,0 +1,24 @@
+package com.timecapsule.app.time.capsule.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String email;
+
+}
