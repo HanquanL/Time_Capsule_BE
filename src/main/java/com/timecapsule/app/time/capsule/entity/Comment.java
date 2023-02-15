c4a9e38b1f5d6e3fe8f9a90361425b81e381ef4a
HanquanL <hanquanl0115@gmail.com>
2023-02-15 17:04:10 -0500

    comment eniy done

---
+++ src/main/java/com/timecapsule/app/time/capsule/entity/Comment.java
@@ -0,0 +1,42 @@
+package com.timecapsule.app.time.capsule.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> replies = new ArrayList<>();
+
+    public Comment(String text, User user, Post post) {
+        this.text = text;
+        this.user = user;
+        this.post = post;
+    }
+}
