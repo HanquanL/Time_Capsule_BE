17a251045ef6b252cbebb51df9d8e67faea6b6ca
HanquanL <hanquanl0115@gmail.com>
2023-02-16 21:32:17 -0500

    voterepository added

--- src/main/java/com/timecapsule/app/time/capsule/repository/CommentRepository.java
+++ src/main/java/com/timecapsule/app/time/capsule/repository/CommentRepository.java
@@ -5,7 +5,8 @@ import com.timecapsule.app.time.capsule.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPost(Post post);
+    Optional<List<Comment>> findByPost(Post post);
 }
--- src/main/java/com/timecapsule/app/time/capsule/repository/VoteRepository.java
+++ src/main/java/com/timecapsule/app/time/capsule/repository/VoteRepository.java
@@ -1,4 +1,12 @@
 package com.timecapsule.app.time.capsule.repository;
 
-public interface VoteRepository {
+import com.timecapsule.app.time.capsule.entity.Post;
+import com.timecapsule.app.time.capsule.entity.User;
+import com.timecapsule.app.time.capsule.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findByPostAndUser(Post post, User user);
 }
