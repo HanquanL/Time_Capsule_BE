79167e2d54c6a0e934f3c4e5f429f8499fe203e3
HanquanL <hanquanl0115@gmail.com>
2023-02-16 21:19:25 -0500

    SubTimeCapsuleRepository added

---
+++ src/main/java/com/timecapsule/app/time/capsule/repository/SubTimeCapsuleRepository.java
@@ -0,0 +1,7 @@
+package com.timecapsule.app.time.capsule.repository;
+
+import com.timecapsule.app.time.capsule.entity.SubTimeCapsule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubTimeCapsuleRepository extends JpaRepository<SubTimeCapsule, Long> {
+}
