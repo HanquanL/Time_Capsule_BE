cb2f098800130578c9c3c98bcfac0a8231171528
HanquanL <hanquanl0115@gmail.com>
2023-02-19 14:27:46 -0500

    MailBuilder added

--- src/main/java/com/timecapsule/app/time/capsule/service/AuthService.java
+++ src/main/java/com/timecapsule/app/time/capsule/service/AuthService.java
@@ -2,12 +2,16 @@ package com.timecapsule.app.time.capsule.service;
 
 import com.timecapsule.app.time.capsule.dto.SignupRequest;
 import com.timecapsule.app.time.capsule.entity.User;
+import com.timecapsule.app.time.capsule.entity.VerificationToken;
 import com.timecapsule.app.time.capsule.repository.UserRepository;
+import com.timecapsule.app.time.capsule.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 import static java.time.Instant.now;
 
@@ -15,6 +19,7 @@ import static java.time.Instant.now;
 @AllArgsConstructor
 @Slf4j
 public class AuthService {
+    private final VerificationTokenRepository verificationTokenRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -28,6 +33,16 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepository.save(user);
+        String token = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user){
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
     }
 
     private String encodePassword(String password) {
